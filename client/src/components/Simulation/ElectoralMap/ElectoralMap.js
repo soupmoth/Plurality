6f4b268c533ec28b5460d421b0d26fa5b83cac1f
soupmoth <gdsygs@gmail.com>
2022-07-10 09:41:04 +0100

    Early stages of Runoff Implementation
    Currently "works", but the way it does so is pretty naive and seems to be bugged? will need to do further testing, but it draws to the screen and has some interesting results

--- client/src/components/Simulation/ElectoralMap/ElectoralMap.js
+++ client/src/components/Simulation/ElectoralMap/ElectoralMap.js
@@ -37,36 +37,113 @@ const ElectoralMap = ({electionParams, seats, setSeatData}) => {
 
   console.log(electionParams);
 
+
+
+  //TODO - Build a method which finds the results of all grouped constituencies
+  //This means an array of objects with first, an inner array of constituencies as "group",
+  //and then the MPs returned by that group. This allows the map to easily crossreference this
+  //resulting array to resolve the colour of the constituency and the data to present
+  //const resolveResults
+
+  //TODO, create method build groups which builds groups of constituencies, then returns an array
+  //of these groups, which is then used by resolveResults.
+  //const buildGroups
+
+  //build groups works on 4 modes.
+  //INDIVIDUAL - Looks at number of MPs per constituency, groups every constituency to match. 
+  //For Individual, the smallest constituencies are prioritised.
+  //BUROUGH and COUNTY - looks at the counties and burough a Constituency is in, then builds
+  //a group of the constituencies in those spaces
+  //REGION - looks at the regions a constituency is in, and groups them with other similar
+  //constituencies
+  //NATION - all constituencies are linked
+
+  //all constituencies in Northen Ireland are to be excluded for now.
+  
+
   //c being the constituency.
-  const determineWinner = (c) => {
+  const determineWinner = (c, mpNumber) => {
 
     //TODO
     //this solution is really bad, its because of how I formatted the backend. Have a fix in mind
     //for it, but I'll have to test it later. Proof of concept
-    var array = [c.con, c.lab, c.ld, c.brexit, c.green, c.snp,
-      c.pc, c.dup, c.sf, c.sdlp, c.uup, c.alliance, c.other];
-    var highestVote = null 
-
-
+    var array = [{pName: "con", vCount : c.con}, {pName: "lab", vCount : c.lab}, 
+    {pName: "ld", vCount : c.ld}, {pName: "brexit", vCount : c.brexit}, 
+    {pName: "green", vCount : c.green}, {pName: "snp", vCount : c.snp}, 
+    {pName: "pc", vCount : c.pc}, {pName: "dup", vCount : c.dup}, {pName: "sf", vCount : c.sf},
+    {pName: "sdlp", vCount : c.sdlp}, {pName: "uup", vCount : c.uup},
+    {pName: "alliance", vCount : c.alliance}, {pName: "other", vCount : c.other}];
 
     array = array.filter(e => {
-      return e>0
+      return e.vCount>0
     })
 
-
-
-    if (electionParams.typeOfVote === eConsts.PLURALITY) {
-      highestVote = Math.max(...array);
-    }
-    else if (electionParams.typeOfVote == eConsts.RUNOFF) {
-      highestVote = Math.min(...array);
-    }
-    else if (electionParams.typeOfVote == eConsts.LOSER_TAKES_ALL) {
-      highestVote = Math.min(...array);
-    }
-    
     let pID = "";
 
+    if (electionParams.typeOfVote === eConsts.PLURALITY) {
+      pID = pluralityVote(array, true, mpNumber);
+    }
+    else if (electionParams.typeOfVote == eConsts.RUNOFF) {
+      pID = runoffVote(array, mpNumber)
+    }
+    else if (electionParams.typeOfVote == eConsts.LOSER_TAKES_ALL) {
+      pID = pluralityVote(array, false, mpNumber);
+    }
+
+    return pID;
+  };
+
+  //handle winners increments a seat
+  const handleWinners = (pID) => {
+    incrementSeats(pID)
+
+    //pIDs.forEach(pID => {
+    //  incrementSeats(pID)
+    //});
+  }
+
+  const incrementSeats = (pID) => {
+    switch (pID) {
+      case "con":
+        seatCount.party.con = seatCount.party.con+1;
+        break;
+      case "lab":
+        seatCount.party.lab = seatCount.party.lab+1;
+        break;
+      case "ld":
+        seatCount.party.ld = seatCount.party.ld+1;
+        break;
+      case "brexit":
+        seatCount.party.brexit = seatCount.party.brexit+1;
+        break;
+      case "green":
+        seatCount.party.green = seatCount.party.green+1;
+        break;
+      case "snp":
+        seatCount.party.snp = seatCount.party.snp+1;
+        break;
+      case "pc":
+        seatCount.party.pc = seatCount.party.pc+1;
+        break;
+      case "dup":
+        seatCount.party.ni = seatCount.party.ni+1;
+        break;
+      case "sf":
+        seatCount.party.ni = seatCount.party.ni+1;
+        break;
+      case "sdlp":
+        seatCount.party.ni = seatCount.party.ni+1;
+        break;
+      case "uup":
+        seatCount.party.ni = seatCount.party.ni+1;
+        break;
+      case "alliance":
+        seatCount.party.ni = seatCount.party.ni+1;
+        break;
+      default:
+        seatCount.party.other = seatCount.party.other+1;
+        break;
+    }
 
     if (seats.total != seatTotal) {
       seatCount.total = seatCount.total+1
@@ -74,61 +151,184 @@ const ElectoralMap = ({electionParams, seats, setSeatData}) => {
     if ((seatCount.total == seatTotal)) {
       setSeatData(seatCount)
     }
-    
-    
-    
-    switch (highestVote) {
-      case c.con:
-        seatCount.party.con = seatCount.party.con+1;
-        pID = "con";break;
-      case c.lab:
-        seatCount.party.lab = seatCount.party.lab+1;
-        pID = "lab";break;
-      case c.ld:
-        seatCount.party.ld = seatCount.party.ld+1;
-        pID = "ld";break;
-      case c.brexit:
-        seatCount.party.brexit = seatCount.party.brexit+1;
-        pID = "brexit";break;
-      case c.green:
-        seatCount.party.green = seatCount.party.green+1;
-        pID = "green";break;
-      case c.snp:
-        seatCount.party.snp = seatCount.party.snp+1;
-        pID = "snp";break;
-      case c.pc:
-        seatCount.party.pc = seatCount.party.pc+1;
-        pID = "pc";break;
-      case c.dup:
-        seatCount.party.ni = seatCount.party.ni+1;
-        pID = "dup";break;
-      case c.sf:
-        seatCount.party.ni = seatCount.party.ni+1;
-        pID = "sf";break;
-      case c.sdlp:
-        seatCount.party.ni = seatCount.party.ni+1;
-        pID = "sdlp";break;
-      case c.uup:
-        seatCount.party.ni = seatCount.party.ni+1;
-        pID = "uup";break;
-      case c.alliance:
-        seatCount.party.ni = seatCount.party.ni+1;
-        pID = "alliance";break;
-      default:
-        seatCount.party.other = seatCount.party.other+1;
-        pID = "other"; break;
-
-    }
-
-
-    return pID;
-    
-  };
+  }
 
   const getColour = (pID) => {
     const result = parties.find(party => party.partyID == pID)
     return result.primaryColour
   };
+
+  const pluralityVote = (partyResults, winnerWins, mpNumber) => {
+    var rawResults = partyResults.map(p => {
+      let resultsTemp = p.vCount
+      return resultsTemp
+    })
+
+    var targetVote = null
+
+    if (winnerWins == true) {
+        targetVote = Math.max(...rawResults);
+    }
+    else {
+        targetVote = Math.min(...rawResults);
+    }
+
+    const winners = partyResults.find((p) => p.vCount === targetVote).pName
+
+
+    handleWinners(winners)
+    
+    try {
+      return winners
+    }
+    catch (error) {
+      console.log(error)
+      return "other"
+    }
+    
+  }
+
+
+  //Runoff voting. Works by reallocating votes to a different party if the
+  //prime party does not win. SMP constituency systems with this are commonly called "AV"
+  //and MMP systems are called "STV". This system ensures no vote is wasted.
+
+  const runoffVote = (partyResults, mpNumber) => {
+    var totalVotes = 0
+    partyResults.map(p => {
+      totalVotes = totalVotes + p.vCount
+    })
+
+    var winProportion = 0.5
+    if (mpNumber > 2) {
+      winProportion = 1/mpNumber
+    }
+
+    var portionedResults = partyResults
+
+    portionedResults.map(p => {
+      p.vCount = p.vCount/totalVotes
+    })
+
+
+    //correcting tactical vote. we assume there are no tactical votes in Runoff
+    portionedResults = redistrubuteTacticalVotes(portionedResults)
+
+    var runoffResults = []
+    portionedResults.forEach(p => {
+      for (let i = 0; i < mpNumber; i++) {
+        runoffResults.push({pName: p.pName, vCount: p.vCount/mpNumber})
+      }
+    });
+
+    var mpsSelected = 0;
+    //while we still have not selected enough MPs.
+    var mostPopular = null
+    while (mpsSelected < mpNumber) {
+      
+      if (runoffResults.length==1) {
+        handleWinners(runoffResults[0].pName)
+        mpsSelected++
+        return runoffResults[0].pName
+      }
+      else {
+        //find the biggest winner
+      runoffResults.forEach(p => {
+        if (mostPopular == null) {
+          mostPopular = p;
+        }
+        else if (p.vCount > mostPopular.vCount) {
+          mostPopular = p;
+        }
+      });
+      //does the biggest winner qualify for a seat yet?
+      if (mostPopular.vCount >= winProportion) {
+        handleWinners(mostPopular.pName)
+        runoffResults = runoffRedistrubtion(runoffResults, mostPopular)
+        mpsSelected++
+        return 
+      }
+      else {
+        var leastPopular = null
+        //find the biggest loser
+          runoffResults.forEach(p => {
+            if (leastPopular == null) {
+              leastPopular = p;
+            }
+            else if (p.vCount < leastPopular.vCount) {
+              leastPopular = p;
+            }
+          });
+
+          runoffResults = runoffRedistrubtion(runoffResults, leastPopular)
+      }
+      }
+      
+    }
+    return mostPopular.pName
+  }
+
+  const runoffRedistrubtion = (runoffResults, runoffVictim) => {
+    let newCap = 1-runoffVictim.vCount
+    let normaliseMult = 1/newCap
+
+    runoffResults = runoffResults.filter(p => runoffVictim != p)
+
+    runoffResults.map(r => {
+      r.vCount = r.vCount*normaliseMult
+    })
+
+    return runoffResults
+  }
+
+  //This redistrubutes the votes from the two best parties in a constituency
+  const redistrubuteTacticalVotes = (results) => {
+    var temp = results;
+
+    var topTwoParties = []
+
+    const extractMaxResult = (r) => {
+      var mostPopular = null
+      r.forEach(p => {
+        if (mostPopular == null) {
+          mostPopular = p;
+        }
+        if (p.vCount > mostPopular.vCount) {
+          mostPopular = p;
+        }
+      });
+      topTwoParties.push(mostPopular);
+      r.filter(p => mostPopular != p)
+    }
+
+    extractMaxResult(temp);
+    extractMaxResult(temp);
+
+    let topPartiesMax = 0
+    let topPartiesMult = 0
+    let minorPartiesMax = 0
+    let minorPartiesMult = 0
+    topTwoParties.map(p => {
+      topPartiesMax = p.vCount + topPartiesMax
+    })
+    minorPartiesMax = 1-topPartiesMax
+
+    topPartiesMult = ((1-electionParams.tacticalVoteProportion) * topPartiesMax)
+    minorPartiesMult = ((electionParams.tacticalVoteProportion * topPartiesMax) + minorPartiesMax)/minorPartiesMax
+
+    topTwoParties.map(p => {
+      p.vCount = p.vCount*topPartiesMult
+    })
+
+    temp.map(p => {
+      p.vCount = p.vCount*minorPartiesMult
+    })
+
+    temp.concat(topTwoParties)
+
+    return temp
+
+  }
 
 
   return (
@@ -150,12 +350,12 @@ const ElectoralMap = ({electionParams, seats, setSeatData}) => {
 
               let colour = null
               try {
-                const winner = determineWinner(currentConstit)
+                const winner = determineWinner(currentConstit, electionParams.noOfMPsPerConst)
                 colour = getColour(winner)
               }
               catch (error) {
                 //console.log(geo.properties.NAME)
-                //console.log(error)
+                console.log(error)
                 
                 colour = getColour("other")
               }
