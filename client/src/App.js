5726198eda1f00c7c121e4d59e67b1c91cd00e41
soupmoth <gdsygs@gmail.com>
2022-07-21 00:37:21 +0100

    decorations part 2

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import {getConstituencies, getParties} from './actions/simulation'
 
-import plurality from './images/plurality.jpg'
+import Header from './images/Header.png'
 import useStyles from './styles';
 import SimulationPage from './components/Simulation/SimulationPage';
 
@@ -20,8 +20,7 @@ const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position='static' color = "inherit">
-                <Typography className={classes.heading} variant = "h2" align = "center">Plurality</Typography>
-                <img className={classes.image} src={plurality} alt = "plurality" height="100"/>
+                <img className={classes.image} src={Header} alt = "plurality" height="125"/>
             </AppBar>
             
             <SimulationPage/>
