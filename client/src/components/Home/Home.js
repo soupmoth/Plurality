2d4ca3ae95f64e6c1d7da51d311400cccd4332bd
soupmoth <gdsygs@gmail.com>
2022-08-08 18:53:52 +0100

    banner added to home screen

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,30 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 
-import { Grid, CircularProgress, LinearProgress} from '@material-ui/core'
-
-import { TextField, Button, Typography, Paper, styled } from "@material-ui/core";
+import {Grid, Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core';
 
 import useStyles from './styles.js';
 
 import * as eConsts from '../../const/electionConsts.js'
+
+import Banner from '../../images/Banner.png'
 
 
 const Home = () => {
     const classes = useStyles();
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-            <Typography variant="h1">among us</Typography>
-            </Grid>
-            <Grid item xs={6}>
-                <Typography variant="h6">among us</Typography>
-            </Grid>
-            <Grid item xs={6}>
-                <Typography variant="h6">among us</Typography>
-            </Grid>
-        </Grid>
+        <Card className={classes.card}> 
+            <CardMedia className={classes.mediaMain} image={Banner} title = {"Scrotum"} />
+            <CardContent>
+                <Typography className={classes.title} size="large" variant="h4">Simulate a British election with different voting systems!</Typography>
+            </CardContent>
+        </Card>
     );
 }
 
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -15,4 +15,52 @@ export default makeStyles(() => ({
       image: {
         marginLeft: '50px',
       },
+        mediaMain: {
+          height: 0,
+          paddingTop: '35%',
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          backgroundBlendMode: 'darken',
+        },
+        border: {
+          border: 'solid',
+        },
+        fullHeightCard: {
+          height: '100%',
+        },
+        card: {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          borderRadius: '15px',
+          height: '100%',
+          position: 'relative',
+        },
+        overlay: {
+          position: 'absolute',
+          top: '20px',
+          left: '20px',
+          color: 'white',
+        },
+        overlay2: {
+          position: 'absolute',
+          top: '20px',
+          right: '20px',
+          color: 'white',
+        },
+        grid: {
+          display: 'flex',
+        },
+        details: {
+          display: 'flex',
+          justifyContent: 'space-between',
+          margin: '20px',
+        },
+        title: {
+          padding: '0 16px',
+        },
+        cardActions: {
+          padding: '0 16px 8px 16px',
+          display: 'flex',
+          justifyContent: 'space-between',
+        },
 }));
